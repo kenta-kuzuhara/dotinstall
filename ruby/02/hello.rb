7e23ccd9a431cd506b00eb5e3fef805daa7e25a5
Kenta Kuzuhara <kuzuhara.1989@gmail.com>
2014-02-26 20:15:19 +0900

    Add all files

---
+++ ruby/02/hello.rb
@@ -0,0 +1,16 @@
+print "Hello world\n" #Normal
+puts "hello worls\n" #Kaigyou de hyouji
+p "hello worlds\n" #Object gata ga wakaruyouni hyouji
+
+#Object
+#MVC model
+x = Rational(2, 3)
+print x
+print "\n"
+
+p "10".to_s
+p 10.4.to_r
+
+p rand(200)
+
+p Math.sqrt(2)
